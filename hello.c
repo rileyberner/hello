14d0e795e93fc886c43a78303efa8eed4947c5a7
Riley Berner <rileyberner@gmail.com>
2019-01-17 12:54:50 -0600

    Create hello.c
    Added 2 lines to print my name and my major.

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Riley Berner
+ * Date: 2019/17/01
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Riley Berner\n");
+  printf("Undecided\n");
+
+  return 0;
+}
